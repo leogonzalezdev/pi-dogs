8620267139243f3218df9640ee216c37df9fcb19
leogonzalezok <leogonzalez777.lg@gmail.com>
2022-08-12 17:16:42 -0300

    Deploy version api

--- api/index.js
+++ api/index.js
@@ -1,16 +1,21 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const axios = require("axios");
-const { Raza, Temperamento } = require("./src/db.js");
+const port = process.env.PORT || 5000
+const {Raza, Temperamento} = require('./src/db.js')
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
+
   let tempsObj = {};
 
-  axios.get(`https://api.thedogapi.com/v1/breeds`).then((response) => {
+  axios.get(`https://api.thedogapi.com/v1/breeds`)
+  .then((response) => {
     response.data?.forEach((breed) => {
       let temperamentos = breed.temperament?.split(", ");
-      temperamentos?.forEach((t) => {
-        Temperamento.findOrCreate({ where: { name: t } }).then((t) => {
+      temperamentos?.forEach(t => {
+        Temperamento.findOrCreate(
+          { where: { name: t } })
+          .then((t) => {
           let arrayTemp = tempsObj[breed.id] || [];
           arrayTemp.push(t[0].dataValues.id);
           tempsObj = {
@@ -22,7 +27,7 @@ conn.sync({ force: false }).then(() => {
     });
   });
 
-  server.listen(3001, () => {
-    console.log("%s listening at 3001");
+  server.listen(port, () => {
+    console.log("%s listening at 5000"); // eslint-disable-line no-console
   });
 });
